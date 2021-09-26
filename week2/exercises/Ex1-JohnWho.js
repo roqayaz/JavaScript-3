52d4a5e310581005b63dd23b6c76f23519286612
roq zob <roqzob@gmail.com>
2021-09-26 16:26:19 +0330

    add comment to ex 1

--- week2/exercises/Ex1-JohnWho.js
+++ week2/exercises/Ex1-JohnWho.js
@@ -1,3 +1,11 @@
+/*------------------------------------------------------------------------------
+Rewrite this function, but replace the callback syntax with the Promise syntax:
+- Have the `getAnonName` function return a `new Promise`.
+- If the Promise `resolves`, pass the full name as an argument to resolve with.
+- If the Promise `rejects`, pass an error as the argument to reject with: "You 
+  didn't pass in a first name!"
+------------------------------------------------------------------------------*/
+
 // const getAnonName = (firstName, callback) => {
 //     setTimeout(() => {
 //       if (!firstName)
@@ -11,6 +19,7 @@
   
 //   getAnonName('John', console.log);
 
+//change the code above
   const getAnonName = firstName => {
       return new Promise((resolve, reject) => {
           setTimeout(() => {
